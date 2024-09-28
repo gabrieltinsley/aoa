e7ff155924f932d88f2b7b23c0919b98b189f5bd
Gabriel Tinsley <gabrieltinsley@u.boisestate.edu>
2024-09-27 18:25:35 -0600

    aoa complete

--- MethodsToAnalyze.java
+++ MethodsToAnalyze.java
@@ -12,16 +12,27 @@ public class MethodsToAnalyze {
 	 */
 	public static int find(int[] array, int value) {
 		/*
-		 * for-loop intializes i and checks if that is less than array length
-		 * which returns false, so 2 statements happen no matter what
+		 * Breaking down the for loop into intializing i and then the if statement
+		 * checks i is greater than the array length before exiting the loop we clearly
+		 * see the equation.
 		 */
 
+		 // t(0) = 2
+
 		 // t(n) = 2 + n(3) = 3n + 2
-		for (int i = 0; i < array.length; i++) {
-			if (array[i] == value) {
-				return i;
+
+		int i = 0; // 1
+		if(i < array.length) { // 1
+			if(array[i] == value){ // 1
+				return i; // n(3)
 			}
+			i++; // 1
 		}
+		// for (int i = 0; i < array.length; i++) {
+		// 	if (array[i] == value) {
+		// 		return i;
+		// 	}
+		// }
 		return -1;
 	}
 
@@ -39,11 +50,17 @@ public class MethodsToAnalyze {
 		 * so 4 statements happen no matter what
 		 */
 
+		 // t(0) = 4
 		 // t(n) = 4 + n(3 + 1 + n(3/2)) + 3(3n/2) = (3/2)n^2 + (15/2)n + 4
+
 		int index = find(array, oldValue);
-		while (index > -1) {
-			array[index] = newValue;
-			index = find(array, oldValue);
+		// This looks like
+		 // t worst(n) = 3n + 2 before returning -1
+		 // t avg(n) = (3/2)n + 2 before returning i
+
+		while (index > -1) { // 1
+			array[index] = newValue; // 1
+		 	index = find(array, oldValue); // (3/2)n + 2
 		}
 	}
 	
@@ -52,14 +69,35 @@ public class MethodsToAnalyze {
 	 * @param array ints that need to be ordered 
 	 */
 	public static void sortIt(int[] array) {
-		for (int next = 1; next < array.length; next++) {
-			int value = array[next];
-			int index = next;
-			while (index > 0 && value < array[index - 1]) {
-				array[index] = array[index - 1];
-				index--;
-			}
-			array[index] = value;
+
+	/*
+	 * The int next is intialized to 1, then next is checked if less
+	 * than array length. If n == 0 or n == 1 than the second statement
+	 * returns false. That means there are two statements that happen no
+	 * matter what.
+	 */
+
+	 // t(0 or 1) = 2
+
+	int next = 1; // 1 
+	if(next < array.length) { // n(5) + 2 = n(n/2) + 5n + 2 = n^2/2 +5n + 2
+		int value = array[next]; // 1
+		int index = next; // 1 
+		while (index > 0 && value < array[index - 1]) { // 2n + 2 // n/2 + 2
+			array[index] = array[index - 1]; // 1
+			index--; // 1
 		}
+		array[index] = value; // 1
+		next++; // 1
+	}
+	// for (int next = 1; next < array.length; next++) {
+	// 		int value = array[next];
+	// 		int index = next;
+	 		//while (index > 0 && value < array[index - 1]) {
+	// 			array[index] = array[index - 1];
+	// 			index--;
+	// 		}
+	// 		array[index] = value;
+	// 	}
 	}
 }
